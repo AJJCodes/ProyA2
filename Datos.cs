c2dbacea5125ab288ec3ad07668a5c3709e5a896
Anthony Jackson <anthonyj300@yahoo.com>
2023-01-30 00:05:24 -0600

    se añadio la clase datos en el cual se le creo las 2 listas para ocupar en el combobox

--- Datos.cs
+++ Datos.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace ProyA2
 {
-    public class Datos
+     class Datos
     {
-
+        //Declarar listas para ocupar en el combobox
+        private List<string> ListaFacul = new List<string>();
+        private List<string> Listasexo = new List<string>();
+        public Datos()
+        {
+            ListaFacul.Add("FEC");
+            ListaFacul.Add("FARQ");
+            ListaFacul.Add("FIQ");
+            ListaFacul.Add("FCM");
+            Listasexo.Add("Masculino");
+            Listasexo.Add("Femenino");
+            Listasexo.Add("No binario");
+        }
     }
 }
